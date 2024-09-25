5f28320842b02c8258a2fe7ae8b27d520d5fa7ce
Kritika Singh <kritikasingh00.com@gmail.com>
2024-09-25 14:29:28 +0530

    child table added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 
-  import Image from 'next/image'
-  
-  export default function Login() {
+
+export default function Login() {
   return (
     <div >
-      <Image
-      src="/assets/background.jpg"
-      width={1000}
-      height={1000}
-      alt="Picture of the author"
-    />
 
     </div>
   );
