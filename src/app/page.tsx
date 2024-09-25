10a70c7c2cc040d96c7f847a507fb64e852d4006
Jaysingh0728 <116194546+Jaysingh0728@users.noreply.github.com>
2024-09-25 13:30:23 +0530

    login page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
-
-  import Image from 'next/image'
-  
-  export default function Login() {
+import Image from 'next/image'
+import logo from '../app/assets/logo.png'
+import LoginForm from '@/components/LoginForm'
+import login from '../app/assets/login-image.jpg'
+ 
+export default function Background() {
   return (
-    <div >
+    <main className='flex'>
+      <div className='w-1/2'>
+        <div className='pt-5 pl-5 flex '>
       <Image
-      src="/assets/background.jpg"
-      width={1000}
-      height={1000}
-      alt="Picture of the author"
+      alt="logo"
+      src={logo}
+      className='w-10 h-10'
     />
-
+    <span className='pt-1 font-medium flex text-2xl'>
+      VacciTime 
+    </span>
     </div>
-  );
+    <div>
+      <LoginForm/>
+    </div></div>
+      
+    <div className='w-1/2 h-screen'>
+    <Image className='h-full'
+      alt="login"
+      src={login}
+      />
+    </div>
+    </main>
+    
+  )
 }
