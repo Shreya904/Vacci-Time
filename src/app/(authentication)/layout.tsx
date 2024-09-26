b05ea9a49139a412b4a36470cd8527387c0124ad
Kritika Singh <kritikasingh00.com@gmail.com>
2024-09-26 10:09:38 +0530

    minor

--- src/app/(authentication)/layout.tsx
+++ src/app/(authentication)/layout.tsx
@@ -10,8 +10,8 @@ const LoginLayout = ({
 }>) => {
   return (
     <main className='flex'>
-      <div className='md:w-1/2 w-full flex flex-col justify-between'>
-        <div className='flex items-center justify-center w-fit mt-4 ml-4'>
+      <div className='md:w-1/2 w-full flex flex-col justify-between '>
+        <div className='flex items-center justify-center w-fit h-fit mt-4 ml-4'>
           <Image
             alt="logo"
             src={logo}
@@ -33,7 +33,7 @@ const LoginLayout = ({
         </div>
 
         <div className='text-sm  w-full text-neutral-500 ml-7 my-4'>
-          © Immunoman 2024
+          © Vaccitime 2024
         </div>
       </div>
 
