11358db5b9dd872c0cb3a5c917ab998a1e9b09fd
Kritika Singh <kritikasingh00.com@gmail.com>
2024-09-27 10:14:55 +0530

    error resolve

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,12 +14,11 @@ const firebaseConfig = {
   storageBucket: "vacci-time-4de62.appspot.com",
   messagingSenderId: "835645239449",
   appId: "1:835645239449:web:8eb9fc697db5caea310808",
-  measurementId: "G-P2C5JC4FQT"
+  measurementId: "G-P2C5JC4FQT",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 const db = getFirestore(app);
